28c8a032cbea128fb89fc8ddd812e6d9bc7fce78
Carolina Da Silva <mdasilvh@everis.com>
2019-02-15 12:20:50 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+// import { SidebarService } from 'src/app/services/service.index';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styles: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sidenav-prueba';
+    options: FormGroup;
+
+    title = 'AngularMaterialGettingStarted';
+
+    isMenuOpen = true;
+    contentMargin = 240;
+
+    task: string[] = [
+        'Clearning out my closet', 'Take out trash bins', 'Wash car', 'Tank up the motorcycles', 'Go for flight training'
+    ];
+    constructor (fb: FormBuilder) {
+        this.options = fb.group({
+            bottom: 0,
+            fixed: false,
+            top: 0
+        });
+    }
+
+    onToolbarMenuToggle() {
+        console.log('On toolbar toggled', this.isMenuOpen);
+        this.isMenuOpen = !this.isMenuOpen;
+
+        if (!this.isMenuOpen) {
+            this.contentMargin = 70;
+        } else {
+            this.contentMargin = 240;
+        }
+    }
+    // sidenavEvents(str) {
+    //   console.log(str);
+    // }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,71 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+
+
+
+import {
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatBadgeModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatInputModule,
+
+} from '@angular/material';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [
+        FormsModule,
+        BrowserModule,
+        CommonModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatSidenavModule,
+        MatListModule,
+        MatButtonModule,
+        MatBadgeModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        ReactiveFormsModule,
+        MatCheckboxModule,
+        MatInputModule
+    ],
+    exports: [
+        FormsModule,
+        BrowserModule,
+        CommonModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatSidenavModule,
+        MatListModule,
+        MatButtonModule,
+        MatBadgeModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        ReactiveFormsModule,
+        MatCheckboxModule,
+        MatInputModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
+
